0ea351790ed4da29c11bf2e0e0224997e514008e
Fabian <f.montemayor26@gmail.com>
2014-10-20 14:36:18 -0500

    Date es una struct + funcion para dia de la semana

--- Classes/Date.h
+++ Classes/Date.h
@@ -1,51 +1,31 @@
-#ifndef DATE_H_INCLUDED
-#define DATE_H_INCLUDED
-
-class Date
-{
-private:
-//Attributes.
-	int iDay, iMonth, iYear;	
-public:
-//Constructor.
-	Date();
-//Modify.
-	void setDay(int iDay);
-	void setMonth(int iMonth);
-	void setYear(int iYear);
-//Access.
-	int getDay();
-	int getMonth();
-	int getYear();
+struct Date{
+    int day = 1;
+    int month = 1;
+    int year = 2014;
 };
-Date::Date()
-{
-	iDay=5;
-	iMonth=5;
-	iYear=2014;
+
+string dayOfWeek(Date d){ //regresar dia de la semana
+    static int t[] = {0, 3, 2, 5, 0, 3, 5, 1, 4, 6, 2, 4};
+    int y = d.year;
+    y -= d.month < 3;
+    int day = (y + y/4 - y/100 + y/400 + t[d.month - 1] + d.day) % 7;
+    switch (d){
+        case 0:
+            return "sunday";
+        case 1:
+            return "monday";
+        case 2:
+            return "tuesday";
+        case 3:
+            return "wednesday";
+        case 4:
+            return "thursday";
+        case 5:
+            return "friday";
+        case 6:
+            return "saturday";
+        default:
+            return "";
+    }
+    
 }
-void Date::setDay(int iDay)
-{
-	this->iDay=iDay;
-}
-void Date::setMonth(int iMonth)
-{
-	this->iMonth=iMonth;
-}
-void Date::setYear(int iMonth)
-{
-	this->iMonth=iMonth;
-}
-int Date::getDay()
-{
-	return iDay;
-}
-int Date::getMonth()
-{
-	return iMonth;
-}
-int Date::getYear()
-{
-	return iYear;
-}
-#endif // DATE_H_INCLUDED
