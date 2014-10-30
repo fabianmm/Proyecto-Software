22800df9d86b452e50857b1b2eb8a6e6c16b80a8
Fabian <f.montemayor26@gmail.com>
2014-10-30 10:53:09 -0600

    sobrecarga hora

--- Classes/Hour.h
+++ Classes/Hour.h
@@ -1,4 +1,10 @@
 struct Hour {
     int hour = 0;
     int minutes = 0;
+    void operator=(Hour h);
 };
+
+void Hour::operator=(Hour h){
+    this->hour = h.hour;
+    this->minutes = h.minutes;
+}
