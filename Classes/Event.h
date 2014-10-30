cc803ef362562c57e0a09ca296014d8536e200a6
JuanOrdaz <juan_ordaz52@hotmail.com>
2014-10-30 11:12:05 -0600

    Cambio en clase

--- Classes/Event.h
+++ Classes/Event.h
@@ -9,63 +9,61 @@ private:
     Hour finish;
     int time;
 public:
-    Event()
-    {
-        name = "N/A";
-        day = "N/A";
-        start.setHour(0);
-        start.setMinute(0);
-        finish.setHour(0);
-        finish.setMinute(0);
-        time = 0;
-    }
-    Event(string a, string b, Hour c, Hour d, int e)
-    {
-        name = a;
-        day = b;
-        start = c;
-        finish = d;
-        time = e;
-    }
-    void setName(string a)
-    {
-        name = a;
-    }
-    string getName()
-    {
-        return name;
-    }
-    void setDay(string a)
-    {
-        day = a;
-    }
-    string getDay()
-    {
-        return day;
-    }
-    void setStart(Hour a)
-    {
-        start = a;
-    }
-    void setFinish(Hour a)
-    {
-        finish = a;
-    }
-    void setTime(int a)
-    {
-        time = a;
-    }
-    int getTime()
-    {
-        return time;
-    }
-    void print()
-    {
-        cout << "Event Name: " << name << endl;
-        cout << "Day: " << day << endl;
-        cout << "Starting time: " << start.hour << ":" << start.minute<< endl;
-        cout << "Finish time: " << finish.hour << ":" << finish.minute << endl;
-        cout << "Total time: " << time << endl;
-    }
-    
+    Event();
+    Event(string a, string b, Hour c, Hour d, int e);
+    void setName(string a);
+    string getName();
+    void setDay(string a);
+    string getDay();
+    void setStart(Hour a);
+    void setFinish(Hour a);
+    void setTime(int a);
+    int getTime();
+    void print();
 };
+Event()
+{
+    name = "N/A";
+    day = "N/A";
+    time = 0;
+}
+void Event::setName(string a)
+{
+    name = a;
+}
+string Event::getName()
+{
+    return name;
+}
+void Event::setDay(string a)
+{
+    day = a;
+}
+string Event::getDay()
+{
+    return day;
+}
+void Event::setStart(Hour a)
+{
+    start = a;
+}
+void Event::setFinish(Hour a)
+{
+    finish = a;
+}
+void Event::setTime(int a)
+{
+    time = a;
+}
+int Event::getTime()
+{
+    return time;
+}
+void Event::print()
+{
+    cout << "Event Name: " << name << endl;
+    cout << "Day: " << day << endl;
+    cout << "Starting time: " << start.hour << ":" << start.minute<< endl;
+    cout << "Finish time: " << finish.hour << ":" << finish.minute << endl;
+    cout << "Total time: " << time << endl;
+}
