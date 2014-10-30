49d406240767e129f2381179408a9343da59e9a9
Jibril <j.d.ivan69@gmail.com>
2014-10-29 22:17:47 -0600

    Class Day, added a getEventFromList, just if in case

--- Classes/Day.h
+++ Classes/Day.h
@@ -1,6 +1,51 @@
-#ifndef DAY_H_INCLUDED
-#define DAY_H_INCLUDED
+#include <vector>
+#include "Event.h"
 
-
-
-#endif // DAY_H_INCLUDED
+class Day
+{
+public:
+//Consutructors
+	Day(string sDName, int iFHours);
+	Day();
+//Modify
+	void setDayName(string sDName);
+	void setFreeHours(int iFHours);
+	void setEventList(Event eEvent);
+//Access
+	string getDayName();
+	int getFreHours();
+	Event getEventFromList(int eID);//Returns an specific object of type "Event"(If needed).
+//Attributes
+	string sDayName;
+	int iFreeHours;
+	std::vector<Event> vEventList;
+};
+//Constructors
+Day::Day(){
+	sDayName = "";
+	iFreeHours = 0;
+}
+Day::Day(string sDName, int iFHours){
+	sDayName = sDName;
+	iFreeHours = iFHours;
+}
+//Modify
+void setDayName(string sDName){
+	sDayName = sDName;
+}
+void setFreeHours(int iFHours){
+	iFreeHours = iFHours;
+}
+void setEventList(Event eEvent){
+	vEventList.PUSH_BACK(eEvent);
+}
+//Access
+string getDayName(){
+	return sDayName;
+}
+int getFreHours(){
+	return iFreeHours;
+}
+Event getEventFromList(int eID){
+	return vEventList[eID];
+}
