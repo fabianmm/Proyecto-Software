57b9baa146ae13a883daf0a335759610eae6769e
Jibril <j.d.ivan69@gmail.com>
2014-11-13 21:08:04 -0600

    Uhmmm

--- Itinerary.h
+++ Itinerary.h
@@ -9,28 +9,31 @@ class Itinerary {
 public:
     //Constructor
     Itinerary();
-    Itinerary(string, string, Date, Date);
+    Itinerary(string name, string city, Date arrival, Date departure);
     //Days attribute is public for easier manipulation
     vector<Day> vDays;
     //Access
     string getName(){return sName;};
     string getCity(){return sCity;};
     Date getArrival(){return dArrival;};
+    Date getMiddleDay(){return dMiddleDay;};//Easier access to this day for changesin the itinerary.
     Date getDeparture(){return dDeparture;};
+    string getDayOfWeek(Date dDate){return dayOfWeek(dDate);};
+    Day getDays(int i){return vDays[i];};
     //Modifiers
     void setName(string name){sName=name;};
     void setCity(string city){sCity=city;};
     void setArrival(Date arrival){dArrival=arrival;};
+    void setMiddleDay(Date middleDay){dMiddleDay=middleDay;};//Easier access to this day for changesin the itinerary.
     void setDeparture(Date departure){dDeparture=departure;};
+    void setDays(int i, string sDay){vDays[i].setDayName(sDay);};
     //Other
     int totalFreeHours();
     void print();
 private:
     //Attributes
-    string sName;
-    string sCity;
-    Date dArrival;
-    Date dDeparture;
+    string sName,  sCity;
+    Date dArrival, dMiddleDay ,dDeparture;
 };
 
 Itinerary::Itinerary(){
@@ -42,6 +45,8 @@ Itinerary::Itinerary(string name, string city, Date arrival, Date departure){
     sName = name;
     sCity = city;
     dArrival = arrival;
+    dMiddleDay = arrival;
+    dMiddleDay.day += dMiddleDay.day + 1;
     dDeparture = departure;
 }
 
@@ -49,7 +54,7 @@ Itinerary::Itinerary(string name, string city, Date arrival, Date departure){
 int Itinerary::totalFreeHours(){
     int hours = 0;
     for (int i = 0; i < vDays.size(); i++){
-        hours += vDays[i].getFreeHours();
+        hours += vDays[i].getFreeHours();//Como que no existe? ERROR======!!
     }
     return hours;
 }
