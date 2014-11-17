1ee52d2a16fef93cc9b0a7ee750cbaa1b39559d1
Fabian <f.montemayor26@gmail.com>
2014-11-17 12:19:53 -0600

    Quitar switches, actualizar números de respuestas

--- Main/Proyecto-Software.cpp
+++ Main/Proyecto-Software.cpp
@@ -24,41 +24,25 @@ Itinerary defaultTemplate(int cityNumber, int days, Date arrival, Date departure
             switch (days)
             {
                 case 2:
-                {
                     archEnt.open("SFTemplate2.txt");
-                    
-                    int numberOfEvents;
-                    
-                    for (int i = 0; i < days; i++)
-                    {
-                        archEnt >> numberOfEvents;
-                        defaultItinerary.vDays[i].vEventList.resize(numberOfEvents);
-                        for (int j = 0; j < numberOfEvents; j++)
-                        {
-                            archEnt >> auxHour.hour >> auxHour.minutes;
-                            //cout << auxHour.hour << " " << auxHour.minutes << endl;
-                            defaultItinerary.vDays[i].vEventList[j].setStart(auxHour);
-                            archEnt >> auxHour.hour >> auxHour.minutes;
-                            defaultItinerary.vDays[i].vEventList[j].setFinish(auxHour);
-                            archEnt >> auxInt;
-                            defaultItinerary.vDays[i].vEventList[j].setTime(auxInt);
-                            archEnt.ignore();
-                            getline(archEnt, auxString);
-                            //cout << auxString << endl;
-                            defaultItinerary.vDays[i].vEventList[j].setName(auxString);
-                        }
-                    }
-                    
-                    archEnt.close();
-                    defaultItinerary.print();
-                    return defaultItinerary;
-                }
+                    break;
+                case 3:
+                    archEnt.open("SFTemplate3.txt");
+                    break;
+                case 4:
+                    archEnt.open("SFTemplate4.txt");
+                    break;
+                case 5:
+                    archEnt.open("SFTemplate5.txt");
             }
         }
             
             
             
+            
     }
+    archEnt.close();
+    defaultItinerary.print();
     return defaultItinerary;
 }
 
@@ -77,43 +61,33 @@ int main(){
         //RF4
         cout << "Elija el número de la ciudad a la que desea viajar:\n1. San Francisco\n2. Kyoto\n3. Las Vegas\n4. London\5. Guadalajara\n";
         cin >> cityNumber;
-        while (cityNumber < 1 || cityNumber > 3)
+        while (cityNumber < 1 || cityNumber > 5)
         {
             cout << "Ese no es un número válido, vuelva a ingresar un número: ";
             cin >> cityNumber;
         }
-        
-        switch (cityNumber)
+        //RF5 & RF6
+        cout << "Ingrese la cantidad de días a viajar (entre 2 y 4): ";
+        cin >> travelDays;
+        while (travelDays < 2 || travelDays > 5)
         {
-                //RF5 & RF6
-            case 1:
-            {
-                cout << "Ingrese la cantidad de días a viajar (entre 2 y 4): ";
-                cin >> travelDays;
-                while (travelDays < 2 || travelDays > 4)
-                {
-                    cout << "Ese no es un número válido, ingrese un número entre 2 y 4: ";
-                    cin >> travelDays;
-                }
-                
-                cout << "Ingrese el día de llegada(dd): ";
-                cin >> arrival.day;
-                cout << "Ingrese el mes de llegada(mm): ";
-                cin >> arrival.month;
-                cout << "Ingrese el año de llegada(aaaa): ";
-                cin >> arrival.year;
-                departure.day = arrival.day + travelDays;
-                departure.month = arrival.month;
-                departure.year = arrival.year;
-                
-                //RF7
-                itinerario = defaultTemplate(cityNumber, travelDays, arrival, departure);
-            }
-                
-            default:
-                cout << "Ese no es un número válido, vuelva a ingresar un número: ";
-                cin >> cityNumber;
+            cout << "Ese no es un número válido, ingrese un número entre 2 y 4: ";
+            cin >> travelDays;
         }
+        
+        cout << "Ingrese el día de llegada(dd): ";
+        cin >> arrival.day;
+        cout << "Ingrese el mes de llegada(mm): ";
+        cin >> arrival.month;
+        cout << "Ingrese el año de llegada(aaaa): ";
+        cin >> arrival.year;
+        departure.day = arrival.day + travelDays;
+        departure.month = arrival.month;
+        departure.year = arrival.year;
+        
+        //imprime y guarda el itinerario default
+        itinerario = defaultTemplate(cityNumber, travelDays, arrival, departure);
+        
         
         
         
